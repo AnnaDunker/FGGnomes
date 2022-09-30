91976e3714db4cdfde996d7f4c0a04069281a824
AnnaDunker <113420237+AnnaDunker@users.noreply.github.com>
2022-09-30 12:25:12 +0200

    Enemy spawning now targeting closest player

--- Gnomes 3D/Assets/Scripts/EnemyController.cs
+++ Gnomes 3D/Assets/Scripts/EnemyController.cs
@@ -7,29 +7,41 @@ public class EnemyController : MonoBehaviour
 {
     public float lookRadius = 60f;
 
-    Transform target;
-    Transform target2;
+    List<Transform> targets = new List<Transform>();
     NavMeshAgent agent;
-
+    Transform nearestTarget = null;
 
     // Start is called before the first frame update
-    void Start()
-        
+    void Start()   
     {
-        target = PlayerManager.instance.playerOne.transform;
-        target2 = PlayerManager.instance.playerTwo.transform;
         agent = GetComponent<NavMeshAgent>();
+        targets.Add(PlayerManager.instance.playerOne.transform);
+        targets.Add(PlayerManager.instance.playerTwo.transform);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(target.position, transform.position);
+        nearestTarget = null;
+        foreach(Transform t in targets) // Go through targets
+        {
+            float distanceToThisTarget = Vector3.Distance(transform.position, t.position); // Calculate distance to this target
+            if (nearestTarget == null || distanceToThisTarget < Vector3.Distance(transform.position, nearestTarget.position)) // If this target is closer than the previous nearest target
+            {
+                nearestTarget = t; // Set new nearest target
+            }
+
+        }
+
+        //Debug.Log("Nearest target is " + nearestTarget.name + " (distance: " + Vector3.Distance(transform.position, nearestTarget.position) + ")");
+
+        float distance = Vector3.Distance(nearestTarget.position, transform.position);
 
         if (distance <= lookRadius)
         {
-            agent.SetDestination(target.position);
-            agent.SetDestination(target2.position);
+            agent.SetDestination(nearestTarget.position);
+            
 
             if (distance <= agent.stoppingDistance)
             {
@@ -39,18 +51,12 @@ public class EnemyController : MonoBehaviour
             }
         }
 
-        /*
-        if (distance > lookRadius)
-        {
-            return;
-        }
-        */
-                        
+                              
     }
 
     void FaceTarget ()
     {
-        Vector3 direction = (target.position - transform.position).normalized;
+        Vector3 direction = (nearestTarget.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
