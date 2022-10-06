a63e9b0adeaa70703cc831502593573cdb80c886
MarveGom <marve.gomes@edu.futuregames.nu>
2022-10-06 19:35:01 +0200

    hhjhjh

--- Gnomes 3D/Assets/Scripts/Teleporter.cs
+++ Gnomes 3D/Assets/Scripts/Teleporter.cs
@@ -4,19 +4,9 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-    private bool hasTeleported = false;
-
-    private void Update()
-    {
-        while(hasTeleported == true)
-        {
-            Update();
-        }
-    }
     private void OnTriggerEnter(Collider Teleport)
     {
         Teleport.transform.position = new Vector3(Random.Range(-70f, 70f), Teleport.transform.position.y, Random.Range( -70f, 50f));
-        hasTeleported = true;
     }
 
 }
