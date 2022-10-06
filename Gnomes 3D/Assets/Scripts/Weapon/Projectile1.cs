97e5d4adbd25094cb50b570f29d790e3e96138cb
MarveGom <marve.gomes@edu.futuregames.nu>
2022-10-06 18:06:55 +0200

    dd

---
+++ Gnomes 3D/Assets/Scripts/Weapon/Projectile1.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile1 : MonoBehaviour
+{
+    [SerializeField] private float Speed = 30f;
+    [SerializeField] private Rigidbody projectileBody;
+
+    private bool isActive;
+
+    public void Initialize()
+    {
+        isActive = true;
+        projectileBody.AddForce(transform.forward * 50f + transform.up * 200f);
+    }
+
+    void Update()
+    {
+        if (isActive)
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collisionObject = collision.gameObject;
+        DestructionFree destruction = collisionObject.GetComponent<DestructionFree>();
+
+        
+
+        if (destruction == null)
+        {
+            Destroy(collisionObject);
+        } 
+        
+        Destroy(this.gameObject);
+    }
+
+}
